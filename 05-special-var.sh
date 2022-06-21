79ed9385d668cd01cd2bd2c8c6358b3586e7fc72
Zeeshuzeeshu <zeeshuzeeshu110@gmail.com>
2022-06-21 21:55:25 +0530

    Spcl Var

---
+++ 05-special-var.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo script name = $0
+echo First Input = $1
+echo Second Input = $2
+echo All Inputs = $*
+echo Number of Inputs = $#
+
+# from the 10th value we need to use flower brackets Like ${10}, ${11} etc
